f62b35f321e3bd5dd6633d2239c99af8acd45f32
Ammar Ratnani <ammrat13@gmail.com>
2021-11-03 17:58:08 +0000

    Added LibC dependency and panic handler

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,24 @@
 mod config;
 mod lib;
 
+use std::boxed::Box;
+use std::panic;
+use std::process;
+
 /// Main method for the kernel
 ///
 /// This is the main method for the SUS kernel. As is standard practice in Rust,
 /// most of the work is done by an internal library, and this function simply
 /// calls into that library.
+///
+/// Note that this function does not return a [Result]. This is intentional. We
+/// want this function to just panic and not print any debugging output.
 fn main() {
-    println!("Hello, world!");
+    // Set up a panic handler
+    // This way, we don't give any information
+    panic::set_hook(Box::new(|_| {
+        process::exit(1);
+    }));
+
+    panic!("sdnkfn");
 }
