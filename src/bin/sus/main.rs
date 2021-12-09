3b7d0cb05ca359c11b7432b7bc4a08f621c538c4
Ammar Ratnani <ammrat13@gmail.com>
2021-12-09 20:18:05 +0000

    Added documentation for main

--- src/bin/sus/main.rs
+++ src/bin/sus/main.rs
@@ -1,3 +1,9 @@
+//! User binary for SUS
+//!
+//! Ideally, the user would not invoke the kernel directly. Instead, they would
+//! do so through this intermediate binary. It provides a `sudo`-like interface
+//! for ease of use.
+
 mod config;
 mod option;
 
@@ -7,6 +13,12 @@ use structopt::StructOpt;
 use option::CommandLineOptions;
 use option::Options;
 
+/// The entrypoint of the binary
+///
+/// As is standard practice in Rust, most of the work is done by internal
+/// libraries, and this function simply calls into those libraries.
+/// Specifically, it parses the command line arguments and calls the kernel with
+/// them, printing out errors if there were any.
 fn main() -> Result<(), Box<dyn Error>> {
     Options::parse_options_like(CommandLineOptions::from_args())?.execute()?;
     Ok(())
